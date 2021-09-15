f1cf87839b028e2b21d5c1c730808769bc637d56
Jason Long <jlong5795@gmail.com>
2021-09-15 17:10:43 -0500

    WIP for system restart

---
+++ challenge-project/pages/components/Room.tsx
@@ -0,0 +1,13 @@
+const Room = () => {
+    return (
+        <div>
+            <form>
+                <input type='textarea' name='message' />
+                <button type='submit'>Send</button>
+            </form>
+        </div>
+        
+    )
+};
+
+export default Room;
